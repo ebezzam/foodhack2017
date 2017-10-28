8dd4b6ddbd61dc59817978238cfdbe3af662a68f
pokitoz <florian.depraz@epfl.ch>
2017-10-28 16:35:26 +0100

    Working post method

--- PythonApp/app.py
+++ PythonApp/app.py
@@ -1,30 +1,34 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, json, request
+#from flask.ext.mysql import MySQL
 
+#mysql = MySQL()
 app = Flask(__name__)
 
-@app.route("/")
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'eric'
+app.config['MYSQL_DATABASE_DB'] = 'BucketList'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+#mysql.init_app(app)
+
+
+@app.route('/')
 def main():
-    # return "Hi Florian!"
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/showSignUp')
 def showSignUp():
-    return render_template('signup.html')
+	return render_template('signup.html')
 
-@app.route('/signUp',methods=['POST'])
+@app.route('/signUp',methods=['POST','GET'])
 def signUp():
- 
-    # read the posted values from the UI
-    _name = request.form['inputName']
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
- 
-    # validate the received values
-    if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
-
+	_name = request.form['inputName']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+	print(_name)
+	print(_email)
+	print(_password)
+	return "Error"
 
 if __name__ == "__main__":
-    app.run()
+	app.run(port=5000)
